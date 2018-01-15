854b6225f1bcbf25abf92b2dc7ec04ebcb5a0a77
dennis <dennis866@gmail.com>
2018-01-15 19:52:21 +0800

    .

---
+++ LifeBuildC/Fire18SignUp02_2.aspx
@@ -0,0 +1,187 @@
+﻿<%@ Page Language="C#"  AutoEventWireup="true" CodeBehind="Fire18SignUp02_2.aspx.cs" Inherits="LifeBuildC.Fire18SignUp02_2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>烈火特會線上報到</title>
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <style>
+        .cssA {
+            width: 95%;
+            font-size: 14px;
+            height: 30px;
+            border-radius: 5px;
+        }
+
+        .cssB {
+            font-size: 14px;
+            margin: 10px;
+        }
+
+        .cssC {
+            width: 20%;
+            font-size: 14px;
+            height: 35px;
+            margin: 5px;
+            border-radius: 5px;
+            background-color: #85325d;
+            color: white;
+        }
+
+        /*.cssD {
+            width: 20%;
+            font-size: 14px;
+            height: 35px;
+            margin: 5px;
+            border-radius: 5px;
+            background-color: #fff;
+            color: #5a5e66;
+        }*/
+
+        #mainBg {
+            display: block;
+            width: 100%;
+            height: auto
+        }
+
+        .cssField {
+            text-align: left;
+            margin: 5px;
+        }
+
+        .cssField800 {
+            display: none;
+        }
+    </style>
+    <script>
+
+        $(function () {
+
+            if ($(window).width() > 800) {
+                $(".cssA").css('width', 300);
+                $(".cssField800").removeClass();
+                $(".cssField").css('display', 'none');
+                $("#imgclothSize").css('width', 800);
+            }
+            else
+            {
+                $("#imgclothSize").css('width', $(window).width() - 60);
+                $("#imgclass").css('width', $(window).width() - 60);
+            }
+
+            $("#btnSend,#btnSave,#btnSaveMail").click(function () {
+
+                if ($('#txtGmail').val() != "") {
+                    displayDiv('divLoading');
+                }
+
+            });
+
+        });
+
+        function closeDiv(divId) {
+            document.getElementById(divId).style.display = 'none';
+        }
+
+        function displayDiv(divId) {
+            document.getElementById(divId).style.display = 'block';
+        }
+
+    </script>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <img id="mainBg" src="/js/fire/main.jpg?f4ffa9d7f2bec29dabd0859458678983" alt="烈火特會-禱告與復興">
+        <div>
+            <div style="text-align: center; margin: 5px;">
+                <h2 class="center">2018 烈火特會報名</h2>
+            </div>
+
+            <!--組別-->
+            <div class="cssField">
+                <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母組別</font>
+            </div>
+            <div style="text-align: center; margin: 5px;">
+                <span class="cssField800">
+                    <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母組別</font>&nbsp;&nbsp;&nbsp;
+                </span>
+                <asp:DropDownList CssClass="cssA" ID="dropGroupClass" runat="server" AutoPostBack="True" OnSelectedIndexChanged="dropGroupClass_SelectedIndexChanged">
+                    <asp:ListItem Value="" disabled Selected hidden>請選擇組別</asp:ListItem>
+                    <asp:ListItem Value="0">家庭組弟兄</asp:ListItem>
+                    <asp:ListItem Value="1">家庭組姊妹</asp:ListItem>
+                    <asp:ListItem Value="2">社青</asp:ListItem>
+                    <asp:ListItem Value="3">學生</asp:ListItem>
+                    <asp:ListItem Value="4">傳道人(配偶)</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <!--小組-->
+            <div class="cssField">
+                <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母小組</font>
+            </div>
+            <div style="text-align: center; margin: 5px;">
+
+                <span class="cssField800">
+                    <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母小組</font>&nbsp;&nbsp;&nbsp;
+                </span>
+
+                <asp:DropDownList CssClass="cssA" ID="dropGroupName" runat="server">
+                    <asp:ListItem Value="" disabled Selected hidden>請選擇組別</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <!--姓名-->
+            <div class="cssField">
+                <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母姓名(有報名的)</font>
+            </div>
+            <div style="text-align: center; margin: 5px;">
+
+                <span class="cssField800">
+                    <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">父母姓名(有報名的)</font>&nbsp;&nbsp;&nbsp;
+                </span>
+
+                <asp:TextBox CssClass="cssA" ID="txtEname1" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="cssField">
+                <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">小孩姓名</font>
+            </div>
+            <div style="text-align: center; margin: 5px;">
+
+                <span class="cssField800">
+                    <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">小孩姓名</font>&nbsp;&nbsp;&nbsp;
+                </span>
+
+                <asp:TextBox CssClass="cssA" ID="txtEname2" runat="server"></asp:TextBox>
+            </div>
+
+            <!--生日-->
+            <div class="cssField">
+                <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">小孩生日</font>&nbsp;
+            </div>
+            <div style="text-align: center; margin: 5px;">
+
+                <span class="cssField800">
+                    <font style="color: #fa5555;">*</font>&nbsp;<font style="color: #5a5e66;">小孩生日</font>&nbsp;&nbsp;&nbsp;
+                </span>
+
+                <asp:TextBox CssClass="cssA" ID="txtBirthday" runat="server" MaxLength="10"></asp:TextBox>
+                <br />
+                <font style="color: #fa5555;">範例：1984/9/11</font>
+            </div>
+
+
+            <hr />
+            <div style="text-align: center; margin: 5px;">
+                <asp:Button ID="btnSend" CssClass="cssC" runat="server" Width="100px" Text="報名" OnClick="btnSend_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LifeBuildC/Fire18SignUp02_2.aspx.cs
@@ -0,0 +1,200 @@
+﻿using ADO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LifeBuildC
+{
+    public partial class Fire18SignUp02_2 : System.Web.UI.Page
+    {
+
+        ChcGroupADO group = new ChcGroupADO();
+        FirePassWADO firePass = new FirePassWADO();
+        FireMemberADO fireMem = new FireMemberADO();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void dropGroupClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = group.QueryGroupNameByChcGroup(dropGroupClass.SelectedItem.Text);
+
+            dropGroupName.Items.Clear();
+            foreach (DataRow dr in dt.Rows)
+            {
+                ListItem li = new ListItem();
+                //出輸格式
+                //AA101.永健牧區-永健小組
+                li.Text = dr["GroupID"].ToString() + "." + dr["GroupCName"].ToString() + "-" + dr["GroupName"].ToString();
+                li.Value = dr["GSort"].ToString();
+                dropGroupName.Items.Add(li);
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// 報名
+        /// </summary>
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool IsCheck = true;
+                string ErrMsg = string.Empty;
+                ChkPageData(ref IsCheck, ref ErrMsg);
+
+                if (IsCheck)
+                {
+
+                    //組別
+                    string GroupClass = dropGroupClass.SelectedItem.Text;
+
+                    //小組
+                    string[] arrg = dropGroupName.SelectedItem.Text.Split('.');
+                    string GroupCName = arrg[1].Split('-')[0];
+                    string GroupName = arrg[1].Split('-')[1];
+
+                    //姓名
+                    string Ename = txtEname1.Text.Trim() + "-" + txtEname2.Text.Trim();
+
+                    string PassKey = Ename;
+                    Session["PassKey"] = Ename;
+                    string Birthday = txtBirthday.Text.Trim();
+
+                    try
+                    {
+                        fireMem.InsFireMember(GroupCName, GroupName, GroupClass, Ename, "",
+    "", true, "", true, PassKey, Birthday);
+
+                        Response.Write("<script>location.href='Fire18SignUp03.aspx'</script>");
+                    }
+                    catch
+                    {
+                        Response.Write("<script>alert('已經報名過了，不得重覆報名');location.href='Fire18SignUp.aspx'</script>");
+                    }
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('" + ErrMsg + "');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('無法報名，請洽櫃檯重新領取一組密碼輸入');</script>");
+                Response.Redirect("Fire18SignUp.aspx");
+            }
+
+
+        }
+
+        /// <summary>
+        /// 資料檢查
+        /// </summary>
+        private void ChkPageData(ref bool IsCheck, ref string ErrMsg)
+        {
+
+            if (!IsDate(txtBirthday.Text.Trim().Replace("/", "-")))
+            {
+                IsCheck = false;
+                ErrMsg = "生日格式輸入不正確";
+                return;
+            }
+
+            if (dropGroupClass.SelectedValue == "")
+            {
+                IsCheck = false;
+                ErrMsg = "請選擇組別";
+                return;
+            }
+
+            if (dropGroupName.SelectedValue == "")
+            {
+                IsCheck = false;
+                ErrMsg = "請選擇小組";
+                return;
+            }
+
+            if (txtEname1.Text.Trim() == "")
+            {
+                IsCheck = false;
+                ErrMsg = "父母姓名(有報名的)";
+                return;
+            }
+
+            if (txtEname2.Text.Trim() == "")
+            {
+                IsCheck = false;
+                ErrMsg = "小孩姓名";
+                return;
+            }
+
+        }
+
+
+        /// <summary>
+        /// 驗證手機號碼
+        /// </summary>
+        /// <param name="input">要驗證的字串</param>
+        /// <returns>驗證通過返回true</returns>
+        public static bool IsMobile(string input)
+        {
+            return Regex.IsMatch(input, RegularExp.Mobile);
+        }
+
+        public static bool IsEmail(string input)
+        {
+            return Regex.IsMatch(input, RegularExp.Email);
+        }
+
+        public static bool IsDate(string input)
+        {
+            return Regex.IsMatch(input, RegularExp.Birthday);
+        }
+
+        public struct RegularExp
+        {
+            public const string Chinese = @"^[\u4E00-\u9FA5\uF900-\uFA2D]+$";
+            public const string Color = "^#[a-fA-F0-9]{6}";
+            public const string Date = @"^((((1[6-9]|[2-9]\d)\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\d|3[01]))|(((1[6-9]|[2-9]\d)\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\d|30))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(0?[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))$";
+            public const string DateTime = @"^((((1[6-9]|[2-9]\d)\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\d|3[01]))|(((1[6-9]|[2-9]\d)\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\d|30))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(0?[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d$";
+            public const string Email = @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$";
+            public const string Float = @"^(-?\d+)(\.\d+)?$";
+            public const string ImageFormat = @"\.(?i:jpg|bmp|gif|ico|pcx|jpeg|tif|png|raw|tga)$";
+            public const string Integer = @"^-?\d+$";
+            public const string IP = @"^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$";
+            public const string Letter = "^[A-Za-z]+$";
+            public const string LowerLetter = "^[a-z]+$";
+            public const string MinusFloat = @"^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$";
+            public const string MinusInteger = "^-[0-9]*[1-9][0-9]*$";
+            public const string Mobile = @"^09\d{2}-?\d{3}-?\d{3}$";
+            public const string NumbericOrLetterOrChinese = @"^[A-Za-z0-9\u4E00-\u9FA5\uF900-\uFA2D]+$";
+            public const string Numeric = "^[0-9]+$";
+            public const string NumericOrLetter = "^[A-Za-z0-9]+$";
+            public const string NumericOrLetterOrUnderline = @"^\w+$";
+            public const string PlusFloat = @"^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$";
+            public const string PlusInteger = "^[0-9]*[1-9][0-9]*$";
+            public const string Telephone = @"(\d+-)?(\d{4}-?\d{7}|\d{3}-?\d{8}|^\d{7,8})(-\d+)?";
+            public const string UnMinusFloat = @"^\d+(\.\d+)?$";
+            public const string UnMinusInteger = @"\d+$";
+            public const string UnPlusFloat = @"^((-\d+(\.\d+)?)|(0+(\.0+)?))$";
+            public const string UnPlusInteger = @"^((-\d+)|(0+))$";
+            public const string UpperLetter = "^[A-Z]+$";
+            public const string Url = @"^http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$";
+            public const string Birthday = @"^(19|20)\d{2}-(1[0-2]|0?[1-9])-(0?[1-9]|[1-2][0-9]|3[0-1])$";
+        }
+
+    }
+}
